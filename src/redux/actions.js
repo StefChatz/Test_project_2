1859d6a338d93708542c00ef4c783c389726c07a
stefchatz <sxatzakis@yahoo.gr>
2021-02-02 18:56:33 +0200

    changes

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,14 +1,13 @@
 import { ADD_TODO, TOGGLE_TODO, SET_FILTER } from "./actionTypes";
 
-let nextTodoId = 0;
-
+let nextTodoId = 0
 export const addTodo = content => ({
   type: ADD_TODO,
   payload: {
     id: ++nextTodoId,
     content
   }
-});
+})
 
 export const toggleTodo = id => ({
   type: TOGGLE_TODO,
